3bc494338ba2f37add9932fcb2954b001eb46838
Rodrigo Fierro <rfierro@systel.com.ar>
2024-04-16 13:53:11 -0300

    20240416 - agrego nuevas funciones

---
+++ FxAsignarPlazoDeEntrega.vb
@@ -0,0 +1,49 @@
+sub main
+    stop
+
+    set xView = NewCompoundView(Self, "CENTROCOSTOS", Self.Workspace, nil, true)
+	xView.AddFilter(NewFilterSpec(xView.ColumnFromPath("ACTIVESTATUS"), " = ", "0"))
+	xView.AddBOCol("NOMBRE")
+	xView.AddOrderColumn(NewOrderSpec(xView.ColumnFromPath("NOMBRE"), false))
+
+
+    For Each xCc in xView.viewitems
+        plazoEntrega = 0
+        Select Case xCc.bo.boextension.zona.nombre
+            Case "San Juan"
+                plazoEntrega = 7
+            Case "Buenos Aires - San Isidro"
+                plazoEntrega = 5
+            Case "Santa Fe - Resto de la Provincia"
+                plazoEntrega = 4
+            Case "San Luis - ciudad de San Luis"
+                MsgBox "El centro de costos "& xCc.bo.nombre &" tiene a san luis como zona.", 64, "Informacion"
+            Case "Buenos Aires - Pilar"
+                plazoEntrega = 5
+            Case "Capital Federal"
+                plazoEntrega = 7
+            Case "Buenos Aires - Escobar"
+                plazoEntrega = 5
+            Case "Buenos Aires - Tres de Febrero"
+                plazoEntrega = 5
+            Case "Cordoba - Gran Cordoba"
+                plazoEntrega = 3
+            Case "Buenos Aires - Gran Buenos Aires"
+                plazoEntrega = 5
+            Case Else
+                MsgBox "El centro de costos "& xCc.bo.nombre &" no tiene una zona asignada.", 64, "Informacion"
+        End Select
+
+
+        for each xProveedor in xCc.bo.boextension.proveedores
+            set xPlazoEntrega           = crearbo("UD_PLAZOENTREGA",self)
+            xPlazoEntrega.centrocostos  = xCc.bo
+            xPlazoEntrega.proveedores   = xProveedor.bo
+            xPlazoEntrega.plazoEntrega  = plazoEntrega
+        next
+        plazoEntrega = 0
+    next
+
+    MsgBox "La correccion se realizo correctamente.", 64, "Informacion"
+
+end sub
